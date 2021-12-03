5cd62ec96e823e6e44f66c1615c7eca16f041b41
Nyi-Nyi <nnpyaehan@gmail.com>
2021-12-03 20:09:24 +0630

    Second Commit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var result = 0;
 
 function increase () {
 
-result = result + 1 
+    result += 1 
 document.querySelector("#result").innerHTML = result
 }
 
 function decrease () {
-    result = result - 1
+    result -= 1
     document.querySelector("#result").innerHTML = result
 }
