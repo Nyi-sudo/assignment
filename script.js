0dba9346579d744a9e2b42d5bd89963ac50f3aec
Nyi-Nyi <nnpyaehan@gmail.com>
2021-12-03 15:15:02 +0630

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var result = 0;
+
+
+function increase () {
+
+result = result + 1 
+document.querySelector("#result").innerHTML = result
+}
+
+function decrease () {
+    result = result - 1
+    document.querySelector("#result").innerHTML = result
+}
